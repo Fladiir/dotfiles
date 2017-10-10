c45f576bffdf5edaaf1a04c74de8b3cdde5ec332
Fladiir <bianchi.matteo97@gmail.com>
2017-10-10 18:37:34 +0200

    ooooh 118

--- scripts/load_layoutChat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-i3-msg "workspace 3; append_layout /home/outcst/.i3/workspace-3.json"
-
-
-(pidgin &)
-
--- scripts/load_layoutCode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-i3-msg "workspace 5; append_layout ~/.i3/workspace-5.json"
-
-(termite -t vim -e 'vim -c NERDTreeToggle' &)
-(termite -t console -d ~/Projects &)
